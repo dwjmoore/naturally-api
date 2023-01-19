7919f2854475a8199d7944392b97015525ea27be
David Moore <dwjmoore@gmail.com>
2023-01-19 16:21:30 +0000

    Added pytz package. Received messaged will be timestamped with MST.

--- route_insert_contact_messages.py
+++ route_insert_contact_messages.py
@@ -1,20 +1,19 @@
 import sqlite3
 from datetime import datetime
+from pytz import timezone
+
 
 def insert_messages(message):
-    date = datetime.now().strftime("%m-%d-%Y, %H:%M")
+	date = datetime.now(timezone('MST')).strftime("%m-%d-%Y, %H:%M")
 
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
+	con = sqlite3.connect("database.db")
+	cur = con.cursor()
 
-    cur.execute("INSERT INTO contact_messages (datetime,name,email,subject,message) VALUES (?,?,?,?,?)",
-        (date,
-        message['name'],
-        message['email'],
-        message['subject'],
-        message['message'])
-    )
-    con.commit()
-    con.close
+	cur.execute(
+	 "INSERT INTO contact_messages (datetime,name,email,subject,message) VALUES (?,?,?,?,?)",
+	 (date, message['name'], message['email'], message['subject'],
+	  message['message']))
+	con.commit()
+	con.close
 
-    return "Success!"
+	return "Success!"
